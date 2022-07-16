1b15cd6f227a0c03554711896f3a09c3e71bd2a4
Daniel Workman <dworkman@squiz.net>
2022-07-16 16:31:16 +1000

    finished spotify component updates and youtube component

--- client/src/components/spotify/GetPlaylists.tsx
+++ client/src/components/spotify/GetPlaylists.tsx
@@ -7,28 +7,20 @@ type Props = {};
 const GetPlaylists = (props: Props) => {
   const dispatch = useAppDispatch();
 
+  //useState to control when to display the fetch more Spotify Playlists button
   const [showGetMorePlaylistsButton, setshowGetMorePlaylistsButton] =
     useState(false);
-
-  const [options, setOptions] = useState({
-    limit: 50,
-    offset: 0
-  });
 
   //grab the initial status from the slice to use to prevent multiple re-renders
   let playlistStatus = useAppSelector((state) => state.spotify.status);
   // let playlistErrors = useAppSelector((state) => state.spotify.error);
   let totalPlaylists = useAppSelector((state) => state.spotify.totalPlaylists);
-  // console.log(playlistStatus);
-  // console.log(totalPlaylists);
-  // console.log(playlistErrors);
   //grab the newly set playlists global state from the dispatch to use to render out on the frontend
   const myPlaylists = useAppSelector((state) => state.spotify.playlists);
-  // console.log(myPlaylists);
 
   //check global state of the playlist status and if idle, pull the data from the server
   useEffect(() => {
-    // dispatch(getUserPlaylists(options));
+    const options = { limit: 50, offset: 0 };
     if (playlistStatus === "idle") {
       dispatch(getUserPlaylists(options));
     }
@@ -39,7 +31,10 @@ const GetPlaylists = (props: Props) => {
   useEffect(() => {
     //only show the getMorePlaylistsButton if the next state is greater than the original playlists length
     //and if that length is not equal to the total array size returned from the server
-    if (next > myPlaylists?.length && myPlaylists?.length !== totalPlaylists) {
+    if (
+      nextSpotify > myPlaylists?.length &&
+      myPlaylists?.length !== totalPlaylists
+    ) {
       setshowGetMorePlaylistsButton(true);
     } else {
       setshowGetMorePlaylistsButton(false);
@@ -48,19 +43,21 @@ const GetPlaylists = (props: Props) => {
 
   //create a function using state to handle how many playlists to show on inital load and then on load more button click.
   let initialImagesToShow = 8;
-  const [next, setNext] = useState(initialImagesToShow);
-  const handleMoreImage = () => {
-    setNext(next + 4);
+  const [nextSpotify, setNextSpotify] = useState(initialImagesToShow);
+  const handleGetMoreSpotifyPlaylists = () => {
+    setNextSpotify(nextSpotify + 4);
   };
 
-  const getMorePlaylists = () => {
-    // setOptions({ limit: 50, offset: +50 });
-    const nextSetOfPlaylists = { limit: 50, offset: 50 };
+  const [counter, setCounter] = useState(50);
+  const getMoreSpotifyPlaylists = () => {
+    //on each button click, increase counter by 50 so that the next dispatch will use the correct offset
+    setCounter(counter + 50);
+    const newOptions = { limit: 50, offset: counter };
+    //ensure that no dispatch is sent if the total amount of playlists available equals the current array length
     if (totalPlaylists !== myPlaylists?.length) {
-      dispatch(getUserPlaylists(nextSetOfPlaylists));
+      dispatch(getUserPlaylists(newOptions));
     }
   };
-  console.log(options);
 
   return (
     <>
@@ -68,7 +65,7 @@ const GetPlaylists = (props: Props) => {
       <div className="spotify">
         {/* on initial load, only display 8 images.
       on button click, increase next state which triggers a re-render, then the slice will slice from 0 - 8 + 4 = 12 to display */}
-        {myPlaylists?.slice(0, next)?.map((list, index) => {
+        {myPlaylists?.slice(0, nextSpotify)?.map((list, index) => {
           //deconstruct variables from the list object so that we can type
           const { external_urls, description, name, images, id } = list;
           // https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-7.html#nullish-coalescing
@@ -92,11 +89,14 @@ const GetPlaylists = (props: Props) => {
       </div>
       {/* only show the button if the number in state is lower than the total array length */}
       <div className="spotify__grid--loadmore">
-        {next < myPlaylists?.length && (
-          <button onClick={handleMoreImage}>Load more</button>
+        {nextSpotify < myPlaylists?.length && (
+          <button onClick={handleGetMoreSpotifyPlaylists}>Load more</button>
         )}
         {showGetMorePlaylistsButton && (
-          <button id="getMoreSpotifyPlaylistsButton" onClick={getMorePlaylists}>
+          <button
+            id="getMoreSpotifyPlaylistsButton"
+            onClick={getMoreSpotifyPlaylists}
+          >
             Load more
           </button>
         )}
--- client/src/components/youtube/GetYoutubePlaylists.tsx
+++ client/src/components/youtube/GetYoutubePlaylists.tsx
@@ -7,23 +7,53 @@ type Props = {};
 const GetYoutubePlaylists = (props: Props) => {
   const dispatch = useAppDispatch();
 
-  //grab the initial status from the slice to use to prevent multiple re-renders
-  const playlistStatus = useAppSelector((state) => state.youtube.status);
+  const youtubePlaylistStatus = useAppSelector((state) => state.youtube.status);
 
-  //check global state of the playlist status and if idle, pull the data from the server
   useEffect(() => {
-    if (playlistStatus === "idle") {
+    if (youtubePlaylistStatus === "idle") {
       dispatch(getYoutubePlaylists());
     }
   });
 
-  //grab the newly set playlists global state from the dispatch to use to render out on the frontend
-  const myPlaylists = useAppSelector((state) => state.youtube.playlists);
-  //   console.log(myPlaylists);
+  let initialImagesToShow = 4;
+  const [youtubeNext, setYoutubeNext] = useState(initialImagesToShow);
+  const handleShowMoreYoutubePlaylists = () => {
+    setYoutubeNext(youtubeNext + 4);
+  };
+
+  const myYoutubePlaylists = useAppSelector((state) => state.youtube.playlists);
+  // console.log(myYoutubePlaylists);
 
   return (
     <>
-      <div>GetYoutubePlaylists</div>
+      <h2 id="youtube-anchor">Youtube Playlists</h2>
+      <div className="youtube">
+        {myYoutubePlaylists?.slice(0, youtubeNext)?.map((list, index) => {
+          const { snippet, id } = list;
+          const youtubePlaylistURL = `https://www.youtube.com/watch?v=QrR_gm6RqCo&list=${id}`;
+          const { localized, thumbnails } = snippet ?? {};
+          const { description, title } = localized ?? {};
+          const { maxres, high } = thumbnails ?? {};
+          const { url: largeThumbnailURL } = high ?? "";
+          return (
+            <div className="youtube__grid" key={index}>
+              <a href={youtubePlaylistURL}>
+                <img src={largeThumbnailURL} alt={description} />
+                <h3>{title}</h3>
+              </a>
+              <p>{description}</p>
+              <a href={youtubePlaylistURL}>
+                <button>View tracklist</button>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+      <div className="youtube__grid--loadmore">
+        {youtubeNext < myYoutubePlaylists?.length && (
+          <button onClick={handleShowMoreYoutubePlaylists}>Load more</button>
+        )}
+      </div>
     </>
   );
 };
